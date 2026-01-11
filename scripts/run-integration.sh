ddb84f8d1bf2484566a2e302e8e7d61da59bf13b
asifrazartu <asifrazartu@gmail.com>
2026-01-11 07:02:05 +0000

    update the run-integration.sh file

--- scripts/run-integration.sh
+++ scripts/run-integration.sh
@@ -1,6 +1,7 @@
 docker-compose up -d
 echo '🟡 - Waiting for database to be ready...'
 ./scripts/wait-for-it.sh "postgresql://postgres:mysecretpassword@localhost:5432/postgres" -- echo '🟢 - Database is ready!'
+npm install prisma@5 @prisma/client@5
 npx prisma migrate dev --name init
 npm run test
 docker-compose down
